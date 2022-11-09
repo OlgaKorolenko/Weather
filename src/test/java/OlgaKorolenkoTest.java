9bdd0c169569dd0f62a09ffa7159258b64eb231d
OlgaKorolenko <o.e.korolenko@gmail.com>
2022-11-09 16:01:43 +0300

    new test added

--- src/test/java/OlgaKorolenkoTest.java
+++ src/test/java/OlgaKorolenkoTest.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OlgaKorolenkoTest {
 
@@ -28,7 +32,6 @@ public class OlgaKorolenkoTest {
 
         WebElement searthCityField = driver.findElement(
                 By.xpath("//div[@id ='weather-widget']//input[@placeholder= 'Search city']"));
-
         searthCityField.click();
         searthCityField.sendKeys(cityName);
         Thread.sleep(5000);
@@ -43,7 +46,6 @@ public class OlgaKorolenkoTest {
                 By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() ='Paris, FR ']")
         );
         parisFRChouiceIdDropdownMenu.click();
-
         Thread.sleep(5000);
 
         WebElement h2CityCountryHeader = driver.findElement(
@@ -51,10 +53,55 @@ public class OlgaKorolenkoTest {
         );
         Thread.sleep(3000);
         String actualResult = h2CityCountryHeader.getText();
-
         Assert.assertEquals(actualResult,expectedResult);
 
         driver.quit();
 
     }
+//                Шаблон
+//    @Test
+//
+//    public void test_name(){
+//        System.setProperty("webdriver.chrome.driver", "\u202AD:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.quit();
+//    }
+
+//    TC_11_01
+//1.  Открыть базовую ссылку
+//2.  Нажать на пункт меню Guide
+//3.  Подтвердить, что вы перешли на страницу со ссылкой https://openweathermap.org/guide и что title этой страницы
+// OpenWeatherMap API guide - OpenWeatherMap
+
+    @Test
+    public void testGuiderUrAndHeader() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://openweathermap.org/";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+        String expectedResultUrl = "https://openweathermap.org/guide";
+
+        driver.get(url);
+
+        WebElement guideElementInMenu = driver.findElement(
+                By.xpath("//a[@href ='/guide']")
+        );
+
+        Thread.sleep(5000);
+        guideElementInMenu.click();
+
+        String actualResultUrl = driver.getCurrentUrl();
+        String actualResultTitle = driver.getTitle();
+
+        Assert.assertEquals(actualResultUrl,expectedResultUrl);
+        Assert.assertEquals(actualResultTitle,expectedResultTitle);
+
+        driver.quit();
+    }
+
+
 }
+
+
